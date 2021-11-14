3039d580af525b3fa0c05a606e580396c353e143
Linus Callheim <stoltefar@gmail.com>
2021-11-14 18:18:28 +0100

    PlaceBoat med hjälpfunktion roomForBoat klara och testade. roomForBoat är däremot vidrigt lång...

--- labb2/BattleShip.java
+++ labb2/BattleShip.java
@@ -2,12 +2,9 @@ public class BattleShip {
   public static void main(String[] args) {
     Board b1 = new Board(10);
     b1.show();
-    b1.set(5,5,'O');
+    b1.placeBoat(4, 3, 'h');
     b1.show();
-    b1.set(1, 7, 'X');
-    b1.show();
-    System.out.println(b1.get(1,7));
-    b1.clear(5,5);
+    b1.placeBoat(6, 7, 'V');
     b1.show();
   }
 }
--- labb2/Board.java
+++ labb2/Board.java
@@ -55,4 +55,199 @@ public class Board {
     }
     System.out.println();
   }
+
+  public boolean placeBoat(int x, int y, char orient) {
+    if (orient=='v' || orient=='V') {
+      if (x<1 || x>= size || y<2 || y>=size-1) {
+        System.out.println("Denna placering är inte giltig då hela, eller någon del av, skeppet hamnar utanför spelplanen. Försök igen.");
+        return false;
+      } else if (roomForBoat(x, y, orient)) {
+          set(x, y-1, 'O');
+          set(x, y, 'O');
+          set(x, y+1, 'O');
+          return true;
+        } else {
+            System.out.println("Ett tidigare placerat skepp ligger i direkt anslutning till denna placering. Gör om, gör rätt!");
+            return false;
+        }
+      }
+      else if (orient=='h' || orient=='H') {
+        if (y<1 || y>= size || x<2 || x>=size-1) {
+          System.out.println("Denna placering är inte giltig då hela, eller någon del av, skeppet hamnar utanför spelplanen. Försök igen.");
+          return false;
+        } else if (roomForBoat(x, y, orient)) {
+            set(x-1, y, 'O');
+            set(x, y, 'O');
+            set(x+1, y, 'O');
+            return true;
+          } else {
+              System.out.println("Ett tidigare placerat skepp förhindrar denna placering. Gör om, gör rätt!");
+              return false;
+            }
+    }
+    return false;
+  }
+  public boolean roomForBoat(int x, int y, char orient) {
+    if (orient=='v' || orient=='V') {
+      if (y==2) {
+        for (int i=size-4; i<size-1; ++i) {
+          if (x==1) {
+            for (int j=0; j<2; ++j) {
+              if (gameBoard[i][j]=='O') {
+                return false;
+              }
+            }
+          }
+          else if (x==size) {
+            for (int j=size-2; j<size; ++j) {
+              if (gameBoard[i][j]=='O') {
+                return false;
+              }
+            }
+          }
+          else {
+            for (int j=x-2; j<=x; ++j) {
+              if (gameBoard[i][j]=='O') {
+                return false;
+              }
+            }
+          }
+        }
+        return true;
+      }
+      else if (y==size-1) {
+        for (int i=0; i<4; ++i) {
+          if (x==1) {
+            for (int j=0; j<2; ++j) {
+              if (gameBoard[i][j]=='O') {
+                return false;
+              }
+            }
+          }
+          else if (x==size) {
+            for (int j=size-2; j<size; ++j) {
+              if (gameBoard[i][j]=='O') {
+                return false;
+              }
+            }
+          }
+          else {
+            for (int j=x-2; j<=x; ++j) {
+              if (gameBoard[i][j]=='O') {
+                return false;
+              }
+            }
+          }
+        }
+        return true;
+      }
+      else {
+        for (int i=size-y-2; i<size-y+2; ++i) {
+          if (x==1) {
+            for (int j=0; j<2; ++j) {
+              if (gameBoard[i][j]=='O') {
+                return false;
+              }
+            }
+          }
+          else if (x==size) {
+            for (int j=size-2; j<size; ++j) {
+              if (gameBoard[i][j]=='O') {
+                return false;
+              }
+            }
+          }
+          else {
+            for (int j=x-2; j<=x; ++j) {
+              if (gameBoard[i][j]=='O') {
+                return false;
+              }
+            }
+          }
+        }
+        return true;
+      }
+    }
+    else if (orient=='h' || orient=='H') {
+      if (y==size) {
+        for (int i=0; i<2; ++i) {
+          if (x==2) {
+            for (int j=0; j<4; ++j) {
+              if (gameBoard[i][j]=='O') {
+                return false;
+              }
+            }
+          }
+          else if (x==size-1) {
+            for (int j=x-3; j<size; ++j) {
+              if (gameBoard[i][j]=='O') {
+                return false;
+              }
+            }
+          }
+          else {
+            for (int j=x-3; j<=x+1; ++j) {
+              if (gameBoard[i][j]=='O') {
+                return false;
+              }
+            }
+          }
+        }
+        return true;
+      }
+      else if (y==1) {
+        for (int i=size-2; i<size; ++i) {
+          if (x==2) {
+            for (int j=0; j<4; ++j) {
+              if (gameBoard[i][j]=='O') {
+                return false;
+              }
+            }
+          }
+          else if (x==size-1) {
+            for (int j=x-3; j<size; ++j) {
+              if (gameBoard[i][j]=='O') {
+                return false;
+              }
+            }
+          }
+          else {
+            for (int j=x-3; j<=x+1; ++j) {
+              if (gameBoard[i][j]=='O') {
+                return false;
+              }
+            }
+          }
+        }
+        return true;
+      }
+      else {
+        for (int i=size-y-1; i<=size-y+1; ++i) {
+          if (x==2) {
+            for (int j=0; j<4; ++j) {
+              if (gameBoard[i][j]=='O') {
+                return false;
+              }
+            }
+          }
+          else if (x==size-1) {
+            for (int j=x-3; j<size; ++j) {
+              if (gameBoard[i][j]=='O') {
+                return false;
+              }
+            }
+          }
+          else {
+            for (int j=x-3; j<=x+1; ++j) {
+              if (gameBoard[i][j]=='O') {
+                return false;
+              }
+            }
+          }
+        }
+        return true;
+      }
+    }
+    else return false;
+  }
 }
